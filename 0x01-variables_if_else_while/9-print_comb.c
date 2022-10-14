e45927b46e443c52145cf003f19a66f77468830a
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-14 06:57:31 -0700

    Task 9-P.P.P Unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,22 +9,22 @@
 int main(void)
 {
 	int a = '0';
-	
 
-		while (a <= '9')
+
+	while (a <= '9')
+	{
+		putchar(a);
+		if (a == '9')
 		{
-			putchar(a);
-			if (a == '9')
-			{
-				putchar('\n');
-				return (0);
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-				a++;
-			}
+			putchar('\n');
+			return (0);
 		}
-		return (0);
+		else
+		{
+			putchar(',');
+			putchar(' ');
+			a++;
+		}
 	}
+	return (0);
+}
