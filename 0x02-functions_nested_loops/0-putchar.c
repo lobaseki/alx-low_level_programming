d3cdf369368b994a7435b78f992d334ee94ad17f
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-18 05:13:38 -0700

    Task 0_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
 
 int main(void)
 {
-	char *h = "Main\n";
-
-	while (*h)
-	{
-		_putchar(*h);
-		h++;
-	}
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0)
 }
-
