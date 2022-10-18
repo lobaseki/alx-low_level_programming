aa532bc88e7a1ec6ee76eee24f07473a5812c2ca
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-18 07:19:29 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,9 +10,10 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-			 c >= 65 && c >= 90)
-		return (1);
-	return (0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+	return (1);
+}
+return (0);
 }
 
