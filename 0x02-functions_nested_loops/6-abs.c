e2a7b15e455e200af8f2924e23187dc76d2b6560
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-18 07:40:17 -0700

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,15 @@
 
 /**
  * _abs - return the absolute value of a given integer.
- * @i: value use to find the absolute value.
+ * @n: value use to find the absolute value.
  *
  *
  * Return: the absolute value.
  */
 int _abs(int n)
 {
-	if (i < 0)
-		return (-i);
-	return (i);
+	if (n < 0)
+		return (-n);
+	return (n);
 }
 
