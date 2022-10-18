02ef2b0618303ba7de2a3d07613b4b2d46774662
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-18 08:14:13 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * add - adds two integers
+ * @a: value to be used for sum.
+ * @b: second value to be use for sum.
+ *
+ * Return: int (Sum)
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
