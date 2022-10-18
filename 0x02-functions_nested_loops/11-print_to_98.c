4166c1151da6a98f72c53c14d2ef9dafb310c70f
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-18 08:21:25 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * print_to_98 - count the natural numbers from n to 98.
+ * separate with a comma, followed by space and number should be printed accordingly
+ *
+ * @n: input
+ */
+
+void print_to_98(int n)
+{
+	int count;
+
+	if (n > 98)
+		for (count = n; count > 98; --count)
+			printf("%d, ", count);
+	else
+		for (count = n; count < 98; ++count)
+			printf("%d, ", count);
+	printf("98\n");
+}
