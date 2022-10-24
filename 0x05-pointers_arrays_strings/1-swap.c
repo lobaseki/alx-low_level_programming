27d4594ac5fa5bcd51109a82afecc854939c817e
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-24 09:06:41 -0700

    don’t swap horses

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * reset_to_98 - main function.
- * @n: Pointer with the address value.
- *
+ * swap_int - main function.
+ * @a: Pointer with the address value.
+ * @b: Pointer with the address value.
  * Description: This function takes a pointer to an int as parameter
  * and updates the value it points to to 98.
  *
- * Return: none.
+ * Return: nothing.
  */
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 {
-	*n = 98;
+	int i;
+
+	i = *a;
+	*a = *b;
+	*b = i;
 }
