8b7f304d1be10ddb5e54bacb02b7e6ae16cb8817
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-25 05:58:50 -0700

    collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - check for lowercase character.
+ * @a: entero recibo por main.
+ * @b: entero recibido por main
+ * Return: 1 if alphabet c is uppercase
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
