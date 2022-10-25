7318aeff31da99a912e3e8a50b37cdf2d184fcd4
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-25 06:55:06 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - Function
+ * print_most_numbers - prints 01356789\n
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
+
 
 void print_most_numbers(void)
 {
-	int n;
+	char c;
 
-	for (num = 0; num <= 9; ++num)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (num == 2 || num == 4)
-			continue;
-		_putchar(num + 48);
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
 	_putchar('\n');
 }
