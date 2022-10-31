7ac29f8b5f58c9015c8d754542cce0d64027784a
Lauretta Obaseki <lobaseki64@gmail.com>
2022-10-31 06:05:14 -0700

    Task 6-Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_chessboard - print chessboard
+ * print_chessboard - function which prints chessboard
  * @a: 8 by 8 array
+ *
+ * Return: nothing
  */
 
 void print_chessboard(char (*a)[8])
 {
-	int x, i = 0;
+	int i, j;
 
-	for (x = 0; x < 8; x++)
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(a[i][x]);
-		if (x == 7 && i < 7)
+		for (j = 0; j < 8; j++)
 		{
-			i++;
-			x = -1;
-			_putchar('\n');
+			_putchar(a[i][j]);
 		}
+
+		_putchar('\n');
 	}
-	_putchar('\n');
-}  
+}
