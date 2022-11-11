5848ebaabb11a3caeb1836e5e9b77791d176bf44
Lauretta Obaseki <lobaseki64@gmail.com>
2022-11-11 03:45:27 -0800

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,7 +7,7 @@
  * @s2: Second string.
  *
  * Description: Write a function that returns a pointer to
- * a newly allocated space in memory containing 
+ * a newly allocated space in memory containing
  * a copy of the string given as a parameter.
  * Return: The pointer to str.
  */
@@ -27,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 		;
 	for (size2 = 0; s2[size2] != '\0'; size2++) /* string length */
 		;
-	array = malloc(((size) + (size2 + 1)) * sizeof(char)); /* New memory alloc */
+	array = malloc(((size) + (size2 + 1)) * sizeof(char)); /* New memory */
 
 	if (!array)
 	{
@@ -35,11 +35,11 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (i = 0; i < size; i++)
 	{
-		array[i] = s1[i]; /* String is copied based on string given by main */
+		array[i] = s1[i]; /* String is copied using main string */
 	}
 	for (j = 0; j < (size2 + 1); j++)
 	{
-		array[i + j] = s2[j]; /* String is copied based on string given by main */
+		array[i + j] = s2[j]; /* String is copied using main string */
 	}
 	return (array); /* New string is returned */
 }
